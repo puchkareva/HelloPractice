1b43234eda8ea95cfe3234fc3b688fb09dc006df
puchkareva <puchkareva@yandex.ru>
2021-11-04 20:03:52 +0100

    задача с семинара

---
+++ Exm002/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Exm002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Задача 1. Поменять местами 2 элемента
+
+            int a = 1;
+            int b = 3;
+            int temp = a;
+
+            Console.Write ("a = ");
+            Console.Write(a);
+            Console.Write(" b = ");
+            Console.Write(b);
+
+            a = b;
+            b = temp;
+
+            Console.WriteLine();
+            Console.Write("a = ");
+            Console.Write(a);
+            Console.Write(" b = ");
+            Console.Write(b);
+
+            
+
+            // Задача 2. Найти наибольшее из двух чисел заданных чисел
+
+            int a = 2;
+            int b = 5;
+
+            Console.WriteLine ("{0}, {1}", a, b);
+            // Console.WriteLine ($"a = {a} b = {b}");
+
+            if (a < b)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(a);
+            }
+
+           
+
+            // Задача 2. Найти наибольшее из двух случайных чисел
+
+            int a = new Random().Next(1,10);
+            int b = new Random().Next(1,10);
+
+             Console.WriteLine ("{0}, {1}", a, b);
+
+            if (a < b)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(a);
+            }
+
+            
+            // Задача 3. Возведение в квадрат числе от 1 до 10
+
+            int current = 1;
+
+            while (current <=10)
+            {
+                Console.WriteLine($"{current} {current*current}");
+                current ++;
+            }
+
+
+             // Задача 4. Найти все числа от 1 до 100, которые одновременно делятся на 3 и 5
+
+             int start = 1;
+             int end = 100;
+             int current = start;
+
+            while (current <= end)
+            {
+                if((current % 3 == 0) && (current % 5 ==0))
+                {
+                    Console.WriteLine(current);
+                }
+                current++;
+            }
+
+            */
+
+            
+           
+
+           
+
+
+
+
+            
+
+
+        }
+    }
+}
