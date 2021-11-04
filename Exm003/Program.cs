6958d9d44d98077a919b606b758bc29ea9b32ec0
puchkareva <puchkareva@yandex.ru>
2021-11-04 21:17:24 +0100

    add Exm 003

---
+++ Exm003/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exm003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+       jb
+
+
+
+        
+       
+
+
+
+        }
+    }
+}
