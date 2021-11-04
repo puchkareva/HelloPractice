2fe66a5c1007a48ffbbc050613c6182683c35b69
puchkareva <puchkareva@yandex.ru>
2021-11-04 16:38:02 +0100

    add addition and ddivision

--- Exm001/Program.cs
+++ Exm001/Program.cs
@@ -6,14 +6,61 @@ namespace Exm001
     {
         static void Main(string[] args)
         {
+            /*
             // Первая лекция
+            // Hello username
 
             Console.WriteLine("Введите Ваше имя");
             string username = Console.ReadLine();
             Console.Write("Привет, ");
             Console.Write(username);
 
-            
+            // Сложение двух чисел
+
+            int numberA = 1;
+            int numberB = 4;
+            Console.WriteLine(numberA+numberB);
+
+            // или
+
+            int numberC = 2;
+            int numberD = 5;
+            int result = numberC + numberD;
+            Console.WriteLine(result);
+
+
+            // Частное двух чисел
+
+            int numberA = 8;
+            int numberB = 4;
+            int result = numberA / numberB;
+            Console.WriteLine(result);
+
+            double numberC = 10;
+            double numberD = 3;
+            double result = numberC / numberD;
+            Console.WriteLine(result);
+
+
+             // Сложение случайных чисел
+
+             int numberA = new Random().Next(1,10);
+             Console.WriteLine(numberA);
+             int numberB = new Random().Next(20,50);
+             Console.WriteLine(numberB);
+             int result = numberA + numberB;
+             Console.WriteLine(result);
+
+             */
+
+             
+
+
+
+
+
+
+
 
         }
     }
