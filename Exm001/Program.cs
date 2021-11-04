9d70559ca6eee69981029489b8893eb5a973e839
puchkareva <puchkareva@yandex.ru>
2021-11-04 16:06:24 +0100

    add Hello username

---
+++ Exm001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exm001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Первая лекция
+
+            Console.WriteLine("Введите Ваше имя");
+            string username = Console.ReadLine();
+            Console.Write("Привет, ");
+            Console.Write(username);
+
+            
+
+        }
+    }
+}
