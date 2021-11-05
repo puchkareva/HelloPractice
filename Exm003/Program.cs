95f27adb57859a0f4f09fe1ae6cd7739ca80a64b
puchkareva <puchkareva@yandex.ru>
2021-11-05 16:38:30 +0100

    add methods

--- Exm003/Program.cs
+++ Exm003/Program.cs
@@ -6,8 +6,9 @@ namespace Exm003
     {
         static void Main(string[] args)
         {
-            
-       // Задача - Поиск максимума из 9
+
+        /*    
+       // Задача. Поиск максимума из 9
 
        int Max(int arg1, int arg2, int arg3)
        {
@@ -16,7 +17,7 @@ namespace Exm003
            if(result < arg3) result = arg3;
            return result;
        }
-       /*
+       
         int a1 = 5;
         int a2 = 2;
         int a3 = 1;
@@ -32,7 +33,7 @@ namespace Exm003
 
         
 
-        // Задача - Поиск максимума из 9. Массив
+        // Задача. Поиск максимума из 9. Массив
 
         int[] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
 
@@ -40,15 +41,78 @@ namespace Exm003
         Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
         Console.WriteLine(result);
 
+
+        // Задача. Имеется одномерный массив array из n элементов, 
+        // требуется найти элемент массива, равный find
+
+        int[] array = {11, 211, 31, 41, 15, 31, 17, 18, 19};
+        int n = array.Length;
+        int find = 31;
+
+        int index = 0;
+
+        while (index < n)
+        {
+            if(array[index] == find)
+            {
+                Console.WriteLine(index);
+                break;
+            }
+                index++;
+        }
+
         */
 
-        //
-    
-        
+        // Метод заполнения массива и метод печати его на экран
 
-        
-       
 
+        void FillArray(int[] collection)
+        {
+            int length = collection.Length;
+            int index = 0;
+            while (index < length)
+            {
+                 collection[index] = new Random().Next(1, 10);
+                 index++;
+            }
+        }
+
+        void PrintArray (int[] col)
+        {
+            int length = col.Length;
+            int position = 0;
+            while(position < length) 
+            {
+                Console.WriteLine(col[position]);
+                position++;
+            }
+        }
+
+        int IndexOf(int[] collection, int find)
+        {
+            int count = collection.Length;
+            int index = 0;
+            int position = -1; 
+
+            while(index < count)
+            {
+                if(collection[index] == find)
+                {
+                    position = index;
+                    break;
+                }
+                index++;
+            }
+            return(position);
+        }
+        
+        int[] array = new int[10];
+        FillArray(array);
+        PrintArray(array);
+
+        Console.WriteLine();
+        int pos = IndexOf(array, 4);
+        Console.WriteLine(pos);
 
 
         }
