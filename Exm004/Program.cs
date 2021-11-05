39ff65f5108f662b9ab689e3ff711407ae2138b8
puchkareva <puchkareva@yandex.ru>
2021-11-05 16:54:49 +0100

    add methods

--- Exm004/Program.cs
+++ Exm004/Program.cs
@@ -6,7 +6,9 @@ namespace Exm004
     {
         static void Main(string[] args)
         {
-            // Задача 1. Посчитать количество натуральных чисел, 
+            
+        /*    
+        // Задача 1. Посчитать количество натуральных чисел, 
         // не превосходящих заданного числа n, которое
         // делится на k, но не на l
         // делится хотя бы на k или на l
@@ -18,7 +20,6 @@ namespace Exm004
         int k = 8;
         int l = 7;
 
-        /*
 
         while (number <= n)
         {
@@ -63,6 +64,30 @@ namespace Exm004
         Console.WriteLine(count);
 
          */
+
+        // Решение задачи через метод
+
+        int Check(int n, int k, int l)
+        {
+            int count = 0;
+            int number = 1;
+
+            while (number <= n)
+            {
+                bool flag1 = (number % k == 0);
+                bool flag2 = (number % l != 0);
+
+                if (flag1 && flag2)
+                {
+                    count++;
+                }
+                number++;
+            }
+            return count;
+        }
+        Console.WriteLine("count = ");
+        Console.WriteLine(Check(39, 2, 5));
+
         }
     }
 }
