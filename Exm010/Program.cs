f204f5e6caeac7db5a5186bc6d337b8926ed3f82
puchkareva <puchkareva@yandex.ru>
2021-11-09 19:43:47 +0100

    Доп задача

---
+++ Exm010/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exm010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Даны два числа. Показать большее и меньшее число
+            char GetMaxNumber(int num)
+            {
+                string ab = Convert.ToString(num);
+                char a = ab[0];
+                char b = ab[1];
+                if (a > b) {return a;}
+                else {return b;}
+            }
+            int G = new Random().Next(10,100);
+            char result = GetMaxNumber(G);
+            Console.WriteLine($"В числе {G} наибольшая цифра {result}");
+
+        }
+    }
+}
