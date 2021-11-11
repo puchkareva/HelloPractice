907ff0c1b8f4143e21fac5a185ac242010afe800
puchkareva <puchkareva@yandex.ru>
2021-11-11 23:04:15 +0100

    Задачи 23-27

--- Exm011/Program.cs
+++ Exm011/Program.cs
@@ -7,22 +7,40 @@ namespace Exm011
         static void Main(string[] args)
         {
 
-            // Почувствуй себя мидлом
-            // 23. Показать таблицу квадратов чисел от 1 до N 
+            // ================ Почувствуй себя мидлом =============
+            // ============ 23. Показать таблицу квадратов чисел от 1 до N ============= 
 
+            // Решение 1
             // void Pow2(int n)
             // {
             //     for (int i = 1; i <= n; i++)
             //     {
             //         Console.WriteLine($"{i}^2 = {i * i}");
+            //        // Console.WriteLine($"{i}^2 = {Math.Pow(i,2)}");
             //     }
             // }
 
             // Pow2(5);
 
+            // Решение 2
+            // double[] Pow2(int n)
+            // {
+            //     double[] array = new double[n];
+            //     for (int i = 1; i <= n; i++)
+            //     {
+            //         array[i - 1] = Math.Pow(i, 2);
+            //     }
+            //     return array;
+            // }
 
-            // 24. Найти кубы чисел от 1 до N
+            // double[] Sqr = Pow2(5);
+            // for (int index = 0; index < Sqr.Length; index++)
+            //     Console.WriteLine(Sqr[index]);
 
+
+            // ============ 24. Найти кубы чисел от 1 до N =============
+
+            // Решение 1
             // void Pow3(int n)
             // {
             //     for (int i = 1; i <= n; i++)
@@ -31,29 +49,89 @@ namespace Exm011
             //         //Console.WriteLine($"{i}^3 = {Math.Pow(i,3)}");
             //     }
             // }
-
             // Pow3(5);
 
-            double[] FuncPow(int n)
-            {
-                double [] array = new double [n];
-                for(int i = 1; i <= n; i++)
-                {
-                    array[i - 1] = Math.Pow(i, 3);
-                }
-                return array;
-            }
-
-            double [] Cube = FuncPow(10);
-
-            for(int index = 0; index < Cube.Length; index++) Console.WriteLine(Cube[index]);
-
+            // Решение 2
+            // double[] Pow3(int n)
+            // {
+            //     double[] array = new double[n];
+            //     for (int i = 1; i <= n; i++)
+            //     {
+            //         array[i - 1] = Math.Pow(i, 3);
+            //     }
+            //     return array;
+            // }
+            // double[] Cube = Pow3(5);
+            // for (int index = 0; index < Cube.Length; index++) Console.WriteLine(Cube[index]);
 
 
-            // 25. Найти сумму чисел от 1 до А
-            // 26. Возведите число А в натуральную степень B используя цикл
+            // ============ 25. Найти сумму чисел от 1 до А ===============
+
+            // int SumOfNumbers(int a)
+            // {
+            //     int sum = 0;
+            //     for (int i = 1; i <= a; i++)
+            //     {
+            //         sum = sum + i;
+            //     }
+            //     return sum;
+            // }
+            // Console.WriteLine(SumOfNumbers(10));
+
+
+            // ========== 26. Возведите число А в натуральную степень B используя цикл ========
+
+            //  Решение без цикла
+            // double Pow(int a, int b)
+            // {
+            //     return Math.Pow(a, b);
+            // }
+            // Console.WriteLine(Pow(3, 2));
+
+            // double fPow(double numA, int numB)
+            // {
+            //     double result = numA;
+            //     for (int i = 1; i < numB; i++) result = result * numA;
+            //     return result;
+            // }
+            // double a = 2.2;
+            // int b = 3;
+            // Console.WriteLine(fPow(a, b));
+
             // 27. Определить количество цифр в числе
+
+            // Решение 1
+            // int FindNum (int n)
+            // {
+            //     int result = 1;
+            //     int divider = 10;
+            //     for(int i = 2; i <= n; i++)
+            //     {
+            //         if(n / divider !=0)
+            //         {
+            //             divider = divider * 10;
+            //             result = i;
+            //         }
+            //     }
+            //     return result;
+            // }
+            // Console.WriteLine(FindNum(0));
+
+            // Решение 2
+            // int FindNum(int n)
+            // {
+            //     if(n==0) return 1;
+            //     int result = 0;
+            //     while (n != 0) { result++; n /= 10; }
+            //     return result;
+            // }
+            // Console.WriteLine(FindNum(0));
+
+
             // 28. Подсчитать сумму цифр в числе
+
+
+
             // 29. Написать программу вычисления произведения чисел от 1 до N
             // 30. Показать кубы чисел, заканчивающихся на четную цифру
 
